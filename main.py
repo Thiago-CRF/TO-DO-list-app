38d80de4a2a1238922c55a91aa511b0ea498f60b
Thiago <thiacefilho@gmail.com>
2026-03-01 14:12:15 -0300

    implementação das rotas da API, primeira versão final

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 from fastapi import FastAPI, HTTPException, Depends, status
 from sqlalchemy.orm import Session
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jose import jwt, JWTError
 
 import models, schemas, database
@@ -24,7 +24,7 @@ def get_db():
 # esquema de autenticação que diz ao fastAPI que o token pro login é na rota /login
 oauth2_scheme = OAuth2PasswordBearer("login")
 
-# verificação do tokenJWT do usuário
+# verificação do tokenJWT do usuário, para quando requisitar ou salvar as tarefas na lista
 def get_current_user(token: str = Depends(oauth2_scheme), 
                      db: Session = Depends(get_db)):
     credential_exception = HTTPException(
@@ -49,7 +49,14 @@ def get_current_user(token: str = Depends(oauth2_scheme),
         raise credential_exception
     return user
 
-# rotas da API
+# rotas publicas da API
+
+@app.get("/")
+def home():
+    """Rota publica de teste pra API"""
+    return {"message": "API de tarefas rodando."}
+
+# cadastro de usuário
 @app.post("/users",response_model=schemas.User)
 def create_user(user: schemas.CreateUser, db: Session = Depends(get_db)):
     # cancela a operação e sobe um erro se o email já estiver cadastrado
@@ -67,38 +74,40 @@ def create_user(user: schemas.CreateUser, db: Session = Depends(get_db)):
 
     return new_user
 
-@app.get("/")
-def home():
-    """Rota publica de teste pra API"""
-    return {"message": "API de tarefas rodando."}
-
+#login de usuário
 @app.post("/login")
-def login(data: UserLogin):
-    """(teste) Recebe email e senha. se for 'admin' e '123' devolve o token fake"""
-    if data.email == "admin@email.com" and data.password == '123':
-        return {"token": "Exemplo-token-123", "type": "bearer"}
-    else:
-        raise HTTPException(status_code=401, detail="Wrong email or password")
-    
-@app.get("/tasks", response_model= List[Task])
-def list_tasks(token: str = "Exemplo-token-123"):
-    """só mostra as tarefas se tiver o token, tornando a rota protegida
-    (por ser só um teste de estrutura, estou pedindo o token como parametro de proteção)"""
-    # verificação (no futura vai ser automática com Depends)    
-    if token != "Exemplo-token-123":
-        raise HTTPException(status_code=401, detail="Unauthorized")
-    
-    return task_bank
+def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    # busca o usuário com base no email do formulário
+    user = db.query(models.User).filter(models.User.email == form_data.username).first()
+
+    # da erro se a senha não bater com o hash ou não achar o usuário
+    if not user or not auth.verify_password(form_data.password, user.hash_password):
+        raise HTTPException(status.HTTP_401_UNAUTHORIZED,
+                            "E-mail ou senha incorretos",
+                            {"WWW-Authenticate": "Bearer"})
+
+    acess_token = auth.create_JWT_token(data={"sub": user.email})    
+    return {"acess_token": acess_token, "token_type": "bearer"}
+
+
+# rotas privadas (precisa do tokenJWT)
+
+@app.get("/tasks", response_model=list[schemas.Task])
+def list_tasks(db: Session = Depends(get_db), current_user: models.User = Depends(get_current_user)):
+    # verificação do token JWT feita ao chamar get_current_user, quando define current_user
+    # busca todas as tarefas guardadas daquele usuário
+    tasks = db.query(models.Task).filter(models.Task.owner_id == current_user.id).all()
+    return tasks
 
 @app.post("/tasks")
-def create_task(task: Task, token: str = "Exemplo-token-123"):
-    """Rota protegida. Cria uma tarefa na lista"""
-    if token != "Exemplo-token-123":
-        raise HTTPException(status_code=401, detail="Unauthoraized")
-    
-    # salva a tarefa (lógica só pra teste)
-    new_task = task.model_dump()
-    new_task["id"] = len(task_bank) + 1
-    task_bank.append(new_task)
-
-    return {"mensagem": "Tarefa criada", "tarefa": new_task}
+def create_task(task: schemas.CreateTask, db: Session = Depends(get_db), current_user: models.User = Depends(get_current_user)):
+    """Rota protegida. Cria uma tarefa na lista, ligada ao ID do usuário logado"""
+    # ** antes de task.model_dump() faz com que ele transforme automaticamente 
+    # todas as cháves do dicionário em variáveis, quando o model_dump transforma
+    # do formato do pydantic em dicionario python padrão
+    new_task = models.Task(**task.model_dump(), owner_id=current_user.id)
+    db.add(new_task)
+    db.commit()
+    db.refresh(new_task)
+   
+    return new_task
